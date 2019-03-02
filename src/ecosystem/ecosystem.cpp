74c71449f438121273da98b708177ef80390eaa3
Plato <otalpster@gmail.com>
2019-03-03 00:06:31 +0200

    ecosystem.cpp: implement void print_system() const

--- src/ecosystem/ecosystem.cpp
+++ src/ecosystem/ecosystem.cpp
@@ -11,3 +11,11 @@ Ecosystem::Ecosystem(size_t terrain_size, string season)
 		throw "Unknown season provided.";
 	this->season = season;
 }
+void Ecosystem::print_system() const {
+	size_t terrain_size = terrain.get_size();
+	for (size_t c_row = 0; c_row < terrain_size; c_row++) {
+		for (size_t c_col = 0; c_col < terrain_size; c_col++)
+			cout << terrain.get_habitat(c_row, c_col);
+		cout << endl;
+	}
+}
