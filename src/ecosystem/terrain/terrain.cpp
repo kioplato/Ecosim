7dbae2609219493e34305a5d11220cbcaafeb637
Plato <otalpster@gmail.com>
2019-03-02 23:31:49 +0200

    terrain.cpp: implement void generate_river()

--- src/ecosystem/terrain/terrain.cpp
+++ src/ecosystem/terrain/terrain.cpp
@@ -23,3 +23,36 @@ void Terrain::terrain_generator() {
 	generate_hills();
 	generate_meadow();
 }
+
+void Terrain::generate_river() {
+	// River can start from [5, terrain_size - 5]
+	size_t possible_points = terrain_size - 9;
+	size_t start_point = (rand() % possible_points) + 5;
+	tiles[0][start_point].set_habitat('#');
+	size_t current_point = start_point;
+	for (size_t c_row = 1; c_row < terrain_size; c_row++) {
+		tiles[c_row][current_point].set_habitat('#');
+		bool deviate = (rand() % 10 < 3) ? true : false;
+		if (deviate) {
+			bool to_right = rand() % 2;
+			bool twice = rand() % 2;
+			if (to_right) {
+				if (current_point + 1 >= terrain_size - 2) continue;
+				tiles[c_row][current_point + 1].set_habitat('#');
+				current_point ++;
+				if (twice) {
+					if (current_point + 1 >= terrain_size - 2) continue;
+					tiles[c_row][current_point + 1].set_habitat('#'); }
+				current_point += (twice) ? 1 : 0;
+			} else {
+				if (current_point - 3 > terrain_size) continue;
+				tiles[c_row][current_point - 1].set_habitat('#');
+				current_point--;
+				if (twice) {
+					if (current_point - 3 > terrain_size) continue;
+					tiles[c_row][current_point - 1].set_habitat('#'); }
+				current_point += (twice) ? -1 : 0;
+			}
+		}
+	}
+}
