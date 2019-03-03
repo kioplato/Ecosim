07932fbdb0fb0336409de7173dc1564bdcb49291
Plato <otalpster@gmail.com>
2019-03-03 15:44:06 +0200

    terrain.cpp: call terrain_generate() instead of map_generate() in constructor

--- src/ecosystem/terrain/terrain.cpp
+++ src/ecosystem/terrain/terrain.cpp
@@ -7,7 +7,7 @@
 Terrain::Terrain(size_t terrain_size)
 	: tiles(NULL), terrain_size(terrain_size) {
 	if (terrain_size < 10) throw "Terrain size must be at least 10.";
-	map_generator();
+	terrain_generator();
 }
 
 Terrain::~Terrain() {
